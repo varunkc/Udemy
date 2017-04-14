2c6b8e407cbbd0f02aec45b2f630b3c293ea9aeb
varunkumar <chepuriv5271@uhcl.edu>
2017-04-14 18:42:18 -0500

    After Lecture 17
    Modifying the exisitng Bootstrap theme, Displaying list of customers,
    Introduction to Partial Views, Using a partial view.

---
+++ Udemy/Controllers/MoviesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Udemy.Models;
+using Udemy.ViewModels;
+
+namespace Udemy.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer  { Name = "Customer 1" },
+                new Customer  { Name = "Customer 2" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel); 
+        }
+
+        public ContentResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+
+            return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+
+        }
+
+        [Route("movies/released/{year}/ {month:regex(\\d{4}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+
+   
+}
---
+++ Udemy/ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Udemy.Models;
+
+namespace Udemy.ViewModels
+{
+    public class RandomMovieViewModel
+    {
+        public Movie movie { set; get; }
+        public List<Customer> Customers { set; get; }
+    }
+}
